744c2757db015b42028c4381c367ef4ff621360e
Expfail <ivanbermejomelia@gmail.com>
2026-02-20 13:11:41 +0100

    Data-Store y Citas, Parecce quee funciona
    Caducacion

--- composeApp/src/commonMain/kotlin/com/example/nttdata/data/remote/KtorCliente.kt
+++ composeApp/src/commonMain/kotlin/com/example/nttdata/data/remote/KtorCliente.kt
@@ -1,11 +1,14 @@
 package com.example.nttdata.data.remote
 
+import com.example.nttdata.di.DataGraph
 import io.ktor.client.HttpClient
+import io.ktor.client.plugins.HttpResponseValidator
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.plugins.defaultRequest
 import io.ktor.client.plugins.logging.LogLevel
 import io.ktor.client.plugins.logging.Logger
 import io.ktor.client.plugins.logging.Logging
+import io.ktor.client.plugins.HttpCallValidator
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
 
@@ -21,6 +24,15 @@ val cliente = HttpClient {
         logger = object : Logger {
             override fun log(message: String) {
                 println("HTTP Client: $message")
+            }
+        }
+    }
+
+    install(HttpCallValidator) {
+        validateResponse { response ->
+            if (response.status.value == 401) {
+                println("HTTP Client: Detectado 401 - Sesión expirada")
+                DataGraph.gestorSesionPublico.logout()
             }
         }
     }
